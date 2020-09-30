4d8d8c0cf84446449a2398e27e9e52142b085205
abhishekbhave26 <abhishekbhave26@gmail.com>
2020-09-30 01:29:50 -0700

    Added basic testing framework and multiple env

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+var uri;
+console.log("NODE ENV is" ,process.env.NODE_ENV);
+if(process.env.NODE_ENV.toString() == 'dev'){
+  uri = process.env.ATLAS_URI_DEV;
+  console.log("Uri is", uri);
+}
+else{
+  uri = process.env.ATLAS_URI_TEST;
+  console.log("Uri is", uri);
+}
 
-const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
@@ -31,6 +40,8 @@ app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 app.use('/contact', contactRouter);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+
+module.exports = server;
